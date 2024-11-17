95c49f4c4e860c3a695b7516fe5cbf0bfc3dd09f
rahulrayappasamy <rahul.rayappasamy@ibm.com>
2024-11-18 02:18:52 +0530

    Build and deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Home from "./Components/Home";  // Make sure the path is correct for Home.js
 import Filter from "./Components/Filter";  // Make sure the path is correct for Filter.js
 import './Components/styles.css'; // Adjusted path to the correct location
@@ -8,11 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
+      <div>
       <Routes>
         {/* Define routes for Home and Filter pages */}
         <Route path="/" element={<Home />} />
         <Route path="/filter" element={<Filter />} />
       </Routes>
+      </div>
     </Router>
   );
 }
