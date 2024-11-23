2753995ca0d0dcbbfdd32e6051ab6d396278a9eb
rahulrayappasamy <rahul.rayappasamy@ibm.com>
2024-11-23 17:28:09 +0530

    Fix app deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Filter from "./Components/Filter";
 import "./Components/styles.css";
