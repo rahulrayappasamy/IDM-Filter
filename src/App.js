bf5a0305ec5f1572bfd05fdd7bd9d93a958d3bae
rahulrayappasamy <rahul.rayappasamy@ibm.com>
2024-11-23 17:18:56 +0530

    Fix app deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./Components/Home";  // Make sure the path is correct for Home.js
-import Filter from "./Components/Filter";  // Make sure the path is correct for Filter.js
-import './Components/styles.css'; // Adjusted path to the correct location
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Filter from "./Components/Filter";
+import "./Components/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/IDM-Filter">
       <Routes>
-        {/* Define routes for Home and Filter pages */}
         <Route path="/" element={<Home />} />
-        <Route path="/filter" element={<Filter />} />
+        <Route path="/Filter" element={<Filter />} />
       </Routes>
     </Router>
   );
